974f8e9ea62c88f6f26f61c16be40352bd15e5e2
AdityaSoni17 <33126674+AdityaSoni17@users.noreply.github.com>
2020-11-25 14:10:15 +0530

    Update s3VersionTest.py

--- s3VersionTest.py
+++ s3VersionTest.py
@@ -1,99 +1,101 @@
 import boto3
 import json
 import datetime
-#create the low level functional client
-s3_client = boto3.client('s3')
-s3_resource = boto3.resource('s3')
-iam = boto3.resource('iam')
-sts = boto3.client('sts')
-aws_lambda = boto3.client('lambda')
 
+class S3VersionTest:
+    def __init__(self):
+        # create the low level functional client
+        self.s3_client = boto3.client('s3')
+        self.s3_resource = boto3.resource('s3')
+        self.iam = boto3.resource('iam')
+        self.sts = boto3.client('sts')
+        self.aws_lambda = boto3.client('lambda')
+        self.bucket_name = 'generatedbucket'
+        self.bucket_file_name = "first_data.json"
+        self.local_file_name = "data.json"
 
-bucket_name = 'generatedbucket'
-bucket_file_name = "first_data.json"
-local_file_name = "data.json"
-sample_data = json.dumps({
-    'name':'aditya',
-    'work':'python',
-    'skill':['python','html'],
-    'currentAvg': 70
-})
+    def data_Examole(self):
+        self.sample_data = json.dumps({
+            'name': 'aditya',
+            'work': 'python',
+            'skill': ['python', 'html'],
+            'currentAvg': 70
+        })
 
-update_sample_data = json.dumps({
-    'name':'aditya',
-    'work':'python',
-    'skill':['python','html','java','css'],
-    'framework':['springMVC',"HibernateMVC","django","flask"],
-    'currentAvg': 93
-})
-def lambda_handler(event,context):
+        self.update_sample_data = json.dumps({
+            'name': 'aditya',
+            'work': 'python',
+            'skill': ['python', 'html', 'java', 'css'],
+            'framework': ['springMVC', "HibernateMVC", "django", "flask"],
+            'currentAvg': 93
+        })
 
-    clientResponse = s3_client.list_buckets()
-    # fetch the list of exiting bucket
-    bucket = s3_resource.Bucket(bucket_name)
+    def create_bucket(self):
+        # Create bucket syntax
+        bucket = self.s3_client.create_bucket(
+        Bucket = self.bucket_name)
+        return bucket
 
-    #Enabling Bucketversioning
-    versioning = s3_resource.BucketVersioning(bucket_name = bucket_name)
-    bucket.Versioning().enable()
+    def bucket_list(self):
+        clientResponse = self.s3_client.list_buckets()
+        # Print the bucket names one by one
+        print('Printing bucket names...')
+        bucketList = []
+        for bucket in clientResponse['Buckets']:
+            bucketList.append(bucket["Name"])
+            print(f'Bucket Name: {bucket["Name"]}')
+        response = {
+            "status" : 200,
+            "message": bucketList,
+        }
+        return response
 
-    # uplad one json file in s3 bucket
-    put_object = s3_client.put_object(Bucket = bucket_name, Key = 'sample_data.json', Body = sample_data)
+    def current_bucket(self):
+        # fetch the list of exiting bucket
+        bucket = self.s3_resource.Bucket(self.bucket_name)
+        return bucket
 
-    # upload_file = s3_resource.meta.client.upload_file(Filename ='sample_data.json',Bucket =bucket_name,Key=bucket_file_name)
-    print("S3 Put Object content = ", put_object)
+    def bucket_Version(self):
+        # Enabling Bucketversioning
+        versioning = self.s3_resource.BucketVersioning(bucket_name = self.bucket_name)
+        return self.current_bucket().Versioning().enable()
 
-    # update data and put it again
-    updated_put_object = s3_client.put_object(Bucket = bucket_name, Key = 'sample_data.json', Body = update_sample_data)
+    def put_object(self,bucket_name,key,body):
+        object = self.s3_client.put_object(Bucket = bucket_name, Key = key, Body = body)
+        return object
 
-    # upload_file = s3_resource.meta.client.upload_file(Filename ='sample_data.json',Bucket =bucket_name,Key=bucket_file_name)
-    print("S3 Put Object content update = ", updated_put_object)
+    def get_all_version(self):
+        # get all versions
+        all_Versions = self.s3_client.list_object_versions(Bucket = self.bucket_name)
+        for ver in all_Versions:
+            print("\n")
+            # res = s3_client.get_object()
+            print("Versions = ", ver)
+            print("\n")
+        return all_Versions
 
-    #get all versions
-    all_Versions = s3_client.list_object_versions(Bucket=bucket_name)
-    for ver in all_Versions:
-        print("\n")
-        # res = s3_client.get_object()
-        print("Versions = ",ver)
-        print("\n")
-        
-    
-    # Print the bucket names one by one
-    print('Printing bucket names...')
-    bucketList = []
-    for bucket in clientResponse['Buckets']:
-        bucketList.append(bucket["Name"])
-        print(f'Bucket Name: {bucket["Name"]}')
+    def lambda_handler(self,event, context):
+        # fetch the list of exiting bucket
+        bucket =self.current_bucket()
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {"message": bucketList,
-             "bucket":bucket,
-             # "object":object_version,
-             "put object":put_object,
-             "updated put object":updated_put_object,
-             # "updated object version":new_object_version
-             }
-        )
-    }
+        # Enabling Bucketversioning
+        self.bucket_Version()
 
+        # uplad one json file in s3 bucket
+        first_object = self.put_object(bucket_name = self.bucket_name, key = 'sample_data.json', body = self.sample_data)
 
+        # update data and put it again
+        updated_object = self.put_object(bucket_name = self.bucket_name, key = 'sample_data.json',
+                                         body = self.update_sample_data)
 
+        return {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
 
-#Create bucket syntax
-# bucket = s3_client.create_bucket(
-# ACL = 'private' | 'public-read' | 'public-read-write' | 'authenticated-read',
-# Bucket = bucket_name ,
-# CreateBucketConfiguration = {
-#     'LocationConstraint': s3_resource.meta.client.meta.region_name
-# }
-# CreateBucketConfiguration = {
-#     'LocationConstraint': 'ap-south-1'
-# },
-# GrantFullControl = 'string',
-# GrantRead = 'string',
-# GrantReadACP = 'string',
-# GrantWrite = 'string',
-# GrantWriteACP = 'string',
-# ObjectLockEnabledForBucket = True | False
-# )
+                    "bucket": bucket,
+                    "first object": first_object,
+                    "updated object": updated_object,
+                }
+            )
+        }
