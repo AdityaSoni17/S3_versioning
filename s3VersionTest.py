a7f0ede34840ec06780879e11f2908d285702582
AdityaSoni17 <33126674+AdityaSoni17@users.noreply.github.com>
2020-12-02 14:49:42 +0530

    Update s3VersionTest.py
    In this code we just need to set response json file based on request in AWS, versioning is working but problem in response json file need extra knowledge in aws s3.

--- s3VersionTest.py
+++ s3VersionTest.py
@@ -2,97 +2,90 @@ import boto3
 import json
 import datetime
 
-class S3VersionTest:
-    def __init__(self):
-        # create the low level functional client
-        self.s3_client = boto3.client('s3')
-        self.s3_resource = boto3.resource('s3')
-        self.iam = boto3.resource('iam')
-        self.sts = boto3.client('sts')
-        self.aws_lambda = boto3.client('lambda')
-        self.bucket_name = 'generatedbucket'
-        self.bucket_file_name = "first_data.json"
-        self.local_file_name = "data.json"
-        self.sample_data = json.dumps({
-            'name': 'aditya',
-            'work': 'python',
-            'skill': ['python', 'html'],
-            'currentAvg': 70
-        })
+# create the low level functional client
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+iam = boto3.resource('iam')
+sts = boto3.client('sts')
+aws_lambda = boto3.client('lambda')
+bucket_name = 'generatedbucket'
+bucket_file_name = "first_data.json"
+local_file_name = "data.json"
+sample_data = {
+    'name': 'aditya',
+    'work': 'python',
+    'skill': ['python', 'html'],
+    'currentAvg': 70
+}
 
-        self.update_sample_data = json.dumps({
-            'name': 'aditya',
-            'work': 'python',
-            'skill': ['python', 'html', 'java', 'css'],
-            'framework': ['springMVC', "HibernateMVC", "django", "flask"],
-            'currentAvg': 93
-        })
+update_sample_data = {
+    'name': 'aditya',
+    'work': 'python',
+    'skill': ['python', 'html', 'java', 'css'],
+    'framework': ['springMVC', "HibernateMVC", "django", "flask"],
+    'currentAvg': 93
+}
 
-    def create_bucket(self):
-        # Create bucket syntax
-        bucket = self.s3_client.create_bucket(Bucket = self.bucket_name)
-        return bucket
 
-    def bucket_list(self):
-        clientResponse = self.s3_client.list_buckets()
-        # Print the bucket names one by one
-        print('Printing bucket names...')
-        bucketList = []
-        for bucket in clientResponse['Buckets']:
-            bucketList.append(bucket["Name"])
-            print(f'Bucket Name: {bucket["Name"]}')
-        response = {
-            "status" : 200,
-            "message": bucketList,
+def create_bucket():
+    # Create bucket syntax
+    bucket = s3_client.create_bucket(Bucket = bucket_name)
+    return bucket
+
+def bucket_list():
+    clientResponse = s3_client.list_buckets()
+    # Print the bucket names one by one
+    print('Printing bucket names...')
+    bucketList = []
+    for bucket in clientResponse['Buckets']:
+        bucketList.append(bucket["Name"])
+        print(f'Bucket Name: {bucket["Name"]}')
+    response =  bucketList
+    return response
+
+def current_bucket():
+    # fetch the list of exiting bucket
+    bucket = s3_resource.Bucket(bucket_name)
+    return bucket
+
+def bucket_Version():
+    # Enabling Bucketversioning
+    versioning = s3_resource.BucketVersioning(bucket_name = bucket_name)
+    current_bucket().Versioning().enable()
+
+def put_object(bucket_name,key,body):
+    object = s3_client.put_object(Bucket = bucket_name, Key = key, Body = body)
+    return object
+
+def get_all_version():
+    # get all versions
+    all_Versions = s3_client.list_object_versions(Bucket = bucket_name)
+    for ver in all_Versions:
+        print("\n")
+        # res = s3_client.get_object()
+        print("Versions = ", ver)
+        print("\n")
+    return all_Versions
+
+def lambda_handler(event, context):
+    # fetch the list of exiting bucket
+    bucket =current_bucket()
+
+    # Enabling Bucketversioning
+    bucket_Version()
+
+    # uplad one json file in s3 bucket
+    first_object = put_object(bucket_name = bucket_name, key = 'sample_data.json', body = json.dumps(sample_data))
+
+    # update data and put it again
+    updated_object = put_object(bucket_name = bucket_name, key = 'sample_data.json',
+                                         body = json.dumps(update_sample_data))
+    return {
+        "statusCode": 200,
+        "body":
+        {
+            "bucket": bucket,
+            "first object": first_object,
+            "updated object": updated_object,
         }
-        return response
-
-    def current_bucket(self):
-        # fetch the list of exiting bucket
-        bucket = self.s3_resource.Bucket(self.bucket_name)
-        return bucket
-
-    def bucket_Version(self):
-        # Enabling Bucketversioning
-        versioning = self.s3_resource.BucketVersioning(bucket_name = self.bucket_name)
-        self.current_bucket().Versioning().enable()
-
-    def put_object(self,bucket_name,key,body):
-        object = self.s3_client.put_object(Bucket = bucket_name, Key = key, Body = body)
-        return object
-
-    def get_all_version(self):
-        # get all versions
-        all_Versions = self.s3_client.list_object_versions(Bucket = self.bucket_name)
-        for ver in all_Versions:
-            print("\n")
-            # res = s3_client.get_object()
-            print("Versions = ", ver)
-            print("\n")
-        return all_Versions
-
-    def lambda_handler(self,event, context):
-        # fetch the list of exiting bucket
-        bucket =self.current_bucket()
-
-        # Enabling Bucketversioning
-        self.bucket_Version()
-
-        # uplad one json file in s3 bucket
-        first_object = self.put_object(bucket_name = self.bucket_name, key = 'sample_data.json', body = self.sample_data)
-
-        # update data and put it again
-        updated_object = self.put_object(bucket_name = self.bucket_name, key = 'sample_data.json',
-                                         body = self.update_sample_data)
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps(
-                {
-
-                    "bucket": bucket,
-                    "first object": first_object,
-                    "updated object": updated_object,
-                }
-            )
-        }
+    }
